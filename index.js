29cab6dfc0196081ba2628ba859bb520d8b1bf15
Lim Wu Shen <xiao.wsl@gmail.com>
2017-09-02 01:14:48 +0800

    first

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 311fc6b0-685b-4fd0-b49e-c77e46008777,
+    appPassword: 215tbbjeUvDHK0cP1FFs9px
 });
 
 // Listen for messages from users 
